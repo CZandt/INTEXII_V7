abc6c23eadf0b71e0513e2913073bc8075b17da7
CZandt <cole0hardy@gmail.com>
2023-04-10 12:23:23 -0600

    Partial PostGresSql Support

--- Identity/Startup.cs
+++ Identity/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Design;
 
 namespace Identity
 {
@@ -24,7 +26,9 @@ namespace Identity
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<AppIdentityDbContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"]));
+            services.AddDbContext<AppIdentityDbContext>(options => options.UseNpgsql(Configuration["ConnectionStrings:DefaultConnection"]));
+
+           
             services.AddIdentity<AppUser, IdentityRole>().AddEntityFrameworkStores<AppIdentityDbContext>().AddDefaultTokenProviders();
             services.Configure<DataProtectionTokenProviderOptions>(opts => opts.TokenLifespan = TimeSpan.FromHours(10));
             services.ConfigureApplicationCookie(options =>
