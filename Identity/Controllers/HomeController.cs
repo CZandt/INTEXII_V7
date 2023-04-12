84b9745b1d1275c3815c73d9d8b589ed4cc0b8b7
CZandt <cole0hardy@gmail.com>
2023-04-11 18:05:48 -0600

    Test Onnx prediction Put in

--- Identity/Controllers/HomeController.cs
+++ Identity/Controllers/HomeController.cs
@@ -5,6 +5,9 @@ using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using Identity.Models.Mummy;
 using System.Linq;
+using Microsoft.ML.OnnxRuntime;
+using Identity.Models.Onnx;
+using System.Collections.Generic;
 
 namespace Identity.Controllers
 {
@@ -12,10 +15,12 @@ namespace Identity.Controllers
     {
         private UserManager<AppUser> userManager;
         private ebdbContext mummyContext;
-        public HomeController(UserManager<AppUser> userMgr, ebdbContext mumContext)
+        private InferenceSession _session;
+        public HomeController(UserManager<AppUser> userMgr, ebdbContext mumContext, InferenceSession session)
         {
             userManager = userMgr;
-            mummyContext = mumContext;   
+            mummyContext = mumContext;
+            _session = session;
         }
         public async Task<IActionResult> Index()
         {
@@ -31,7 +36,34 @@ namespace Identity.Controllers
 
         public IActionResult Supervised()
         {
-            return View();
+            //Creates the data object to be put in the model
+            MummyData testData = new MummyData();
+
+            //MANUALLY APPLIES ALL THE VALUES
+            testData.squarenorthsouth = (float)200;
+            testData.depth = (float)1.86;
+            testData.southtohead = (float)2.96;
+            testData.squareeastwest = (float)20.0;
+            testData.westtohead = (float)0.69;
+            testData.length = (float)1.66;
+            testData.westtofeet = (float)2.24;
+            testData.southtofeet = (float)2.97;
+
+            //RUNS THE ONNX
+            var result = _session.Run(new List<NamedOnnxValue>
+            {
+                NamedOnnxValue.CreateFromTensor("float_input", testData.AsTensor())
+            });
+
+            //EXTRACTS THE VALUE FROM THE GOOFY AHH OBJECT
+            var TestFinal = result.First().AsEnumerable<string>().First();
+            // STORES IT IN A PREDICTION OBJECT SO IT IS EASIER TO WORK WITH
+            Prediction prediction = new Prediction();
+
+            prediction.PredictedValue = TestFinal;
+
+
+            return View(prediction);
         }
 
         public IActionResult Unsupervised()
---
+++ Identity/Models/Onnx/MummyData.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.ML.OnnxRuntime.Tensors;
+
+namespace Identity.Models.Onnx
+{
+    public class MummyData
+    {
+        public float squarenorthsouth { get; set; }
+        public float depth { get; set; }
+        public float southtohead { get; set; }
+        public float squareeastwest { get; set; }
+        public float westtohead { get; set; }
+        public float length { get; set; }
+        public float westtofeet { get; set; }
+        public float southtofeet { get; set; }
+
+        public Tensor<float> AsTensor()
+        {
+            float[] data = new float[]
+            {
+                squarenorthsouth, depth, southtohead, squareeastwest, westtohead, length, westtofeet, southtofeet
+            };
+
+            int[] dimensions = new int[] { 1, 8 };
+
+            return new DenseTensor<float>(data, dimensions);
+        }
+    }
+}
