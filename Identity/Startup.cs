c97538e90fcdeb6943482d1a1cf7175a3121ef38
CZandt <cole0hardy@gmail.com>
2023-04-13 20:15:20 -0600

    ONNX PRE EMPTIVE CHANGE

--- Identity/Startup.cs
+++ Identity/Startup.cs
@@ -88,7 +88,7 @@ namespace Identity
             });
              
             services.AddSingleton<InferenceSession>(
-                new InferenceSession("head_directioon_model.onnx"));
+                new InferenceSession("wwwroot/head_directioon_model.onnx"));
 
             services.AddControllersWithViews();
             services.AddRazorPages();
